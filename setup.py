e053e82fc61c04ff5432b8db0a47a951e979e5e7
nictrix <nickwillever@gmail.com>
2014-11-09 18:10:46 -0800

    adding necessary openshift changes

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='unlvfml',
+  version='0.1',
+  description='unlvFML',
+  author='Howard',
+  author_email='feelosophy13@gmail.com',
+  url='http://www.python.org/sigs/distutils-sig/',
+  install_requires=['bottle', 'datetime', 'pytz', 'pymongo']
+)
