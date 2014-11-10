f8135c551b480b4ffc4bbe254ec5a0e2eacb5ebb
nictrix <nickwillever@gmail.com>
2014-11-09 18:18:08 -0800

    fixing file after merge

--- setup.py
+++ setup.py
@@ -2,15 +2,8 @@ from setuptools import setup
 
 setup(name='unlvfml',
   version='0.1',
-<<<<<<< HEAD
-  description='unlvFML',
   author='Howard',
   author_email='feelosophy13@gmail.com',
-=======
-  description='OpenShift App',
-  author='Your Name',
-  author_email='example@example.com',
->>>>>>> 106d8c44dd2f4c8b343c0c6f7c439ca67e0214b1
   url='http://www.python.org/sigs/distutils-sig/',
   install_requires=['bottle', 'datetime', 'pytz', 'pymongo']
 )
