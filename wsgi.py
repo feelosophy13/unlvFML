d1276cb16c3f38d3f0fb62a46de51849b48a54d9
nictrix <nickwillever@gmail.com>
2014-11-09 17:48:55 -0800

    updates

--- wsgi.py
+++ wsgi.py
@@ -1,5 +1,13 @@
 import os
 os.chdir(os.path.dirname(__file__))
 
+virtenv = os.environ['OPENSHIFT_PYTHON_DIR'] + '/virtenv/'
+os.environ['PYTHON_EGG_CACHE'] = os.path.join(virtenv, 'lib/python2.6/site-packages')
+virtualenv = os.path.join(virtenv, 'bin/activate_this.py')
+try:
+    execfile(virtualenv, dict(__file__=virtualenv))
+except IOError:
+    pass
+
 import bottle
 application = bottle.default_app()
